3d1da57c7fd10726426e7585ddbecb938b5355a4
Alexandr <96471504+Poolce@users.noreply.github.com>
2023-03-24 15:36:50 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,36 +1,24 @@
-#include <iostream>
 #include <list>
+#include <iostream>
 #include "main.h"
 
-using std::cout;
 
-
-std::list<dot> JarvisSeq(std::list<dot> Dots){
-    std::list<dot> res;
-    //Get first dot
-    dot firstDot = *Dots.begin();
-    for(auto dot:Dots){
-        if((dot.y < firstDot.y)||((dot.y==firstDot.y)&&(dot.x<firstDot.x)))firstDot = dot;
+bool isEQUAL(std::list<dot> A, std::list<dot> B){
+    if(A.size()!=B.size())return false;
+    auto j = B.begin();
+    for(auto i = A.begin();i != A.end(); ++i){
+        if(*i!=*j) return false;
+        ++j;
     }
-    res.push_back(firstDot);
-    ///
-    
-    while (true)
-    {
-        dot next = Dots.back();
-        for(auto dot:Dots){
-            if(getRotate(res.back(),next)>getRotate(res.back(),dot))next = dot;
-        }
-        if (next == firstDot) return res;
-        else{
-            res.push_back(next);
-            Dots.remove(next);
-        }
-    }
-    
+    return true;
 }
 
 
 int main(){
+    std::list<dot> testList{dot(83, 86), dot(77, 15), dot(93, 35), dot(86, 92), dot(49, 21), dot(62, 27), dot(90, 59), dot(63, 26), dot(40, 26), dot(72, 36), dot(11, 68), dot(67, 29), dot(82, 30), dot(62, 23), dot(67, 35), dot(29, 2), dot(22, 58), dot(69, 67), dot(93, 56), dot(11, 42), dot(29, 73), dot(21, 19), dot(84, 37), dot(98, 24), dot(15, 70), dot(13, 26), dot(91, 80), dot(56, 73), dot(62, 70), dot(96, 81), dot(5, 25), dot(84, 27), dot(36, 5), dot(46, 29), dot(13, 57), dot(24, 95), dot(82, 45), dot(14, 67), dot(34, 64), dot(43, 50), dot(87, 8), dot(76, 78), dot(88, 84), dot(3, 51), dot(54, 99), dot(32, 60), dot(76, 68), dot(39, 12), dot(26, 86), dot(94, 39), dot(95, 70), dot(34, 78), dot(67, 1), dot(97, 2), dot(17, 92), dot(52, 56), dot(1, 80), dot(86, 41), dot(65, 89), dot(44, 19), dot(40, 29), dot(31, 17), dot(97, 71), dot(81, 75), dot(9, 27), dot(67, 56), dot(97, 53), dot(86, 65), dot(6, 83), dot(19, 24), dot(28, 71), dot(32, 29), dot(3, 19), dot(70, 68), dot(8, 15), dot(40, 49), dot(96, 23), dot(18, 45), dot(46, 51), dot(21, 55), dot(79, 88), dot(64, 28), dot(41, 50), dot(93, 0), dot(34, 64), dot(24, 14), dot(87, 56), dot(43, 91), dot(27, 65), dot(59, 36), dot(32, 51), dot(37, 28), dot(75, 7), dot(74, 21), dot(58, 95), dot(29, 37), dot(35, 93), dot(18, 28), dot(43, 11), dot(28, 29)};
+    std::list<dot> testRes{dot(93, 0), dot(29, 2), dot(8, 15), dot(3, 19), dot(1, 80), dot(17, 92), dot(24, 95), dot(54, 99), dot(86, 92), dot(96, 81), dot(97, 71), dot(98, 24), dot(97, 2)};
+    std::list<dot> res = JarvisSeq(testList);
+    
 
+    std::cout<<isEQUAL(testRes,res);
 }
--- main.h
+++ main.h
@@ -1,22 +1,96 @@
 #include <cmath>
+#include <iostream>
+#include <list>
+#include <random>
 
-struct dot
-{
+using std::cout;
+
+struct dot {
     double x;
     double y;
     dot(int _x, int _y) : x(_x), y(_y) {}
-    bool operator==(dot b)
-    {
+    bool operator==(dot b) {
         return ((x == b.x) && (y == b.y)) ? (true) : (false);
     }
-    bool operator!=(dot b)
-    {
-        return !operator==(b);
-    }
+    bool operator!=(dot b) { return !operator==(b); }
 };
 
-double getRotate(dot a, dot b){
-    double res = atan2(b.y-a.y, b.x-a.x);
-    if(res<0) res+= 2*M_PI;
+std::list<dot> getRandDots(int size) {
+    std::list<dot> res;
+    for (int i = 0; i < size; i++) {
+        double x = random() % 100;
+        double y = random() % 100;
+        res.push_back(dot(x, y));
+    }
     return res;
+}
+
+void outDots(std::list<dot> dots) {
+    std::cout << "{";
+    for (auto i : dots) {
+        std::cout << "dot(" << i.x << ", " << i.y << "), ";
+    }
+    std::cout << "}\n";
+}
+
+double getRotate(dot a, dot b) {
+    double res = atan2(b.y - a.y, b.x - a.x);
+    if (res < 0) res += 2 * M_PI;
+    return res;
+}
+
+double getLen(dot a, dot b) {
+    return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
+}
+
+double getCos(dot A, dot O, dot B){
+    double mult = (A.x-O.x)*(B.x-O.x)+(A.y-O.y)*(B.y-O.y);
+    double len1 = sqrt(pow(A.x-O.x,2)+pow(A.y-O.y,2));
+    double len2 = sqrt(pow(B.x-O.x,2)+pow(B.y-O.y,2));
+    return mult/(len1*len2);
+}
+
+std::list<dot> JarvisSeq(std::list<dot> Dots) {
+    std::list<dot> res;
+
+    //////////////////////////GET FIRST////////////////////
+    dot first = *Dots.begin();
+    for (auto dot : Dots) {
+        if (first.y > dot.y) first = dot;
+        if ((first.y == dot.y) && (first.x > dot.x)) first = dot;
+    }
+    res.push_back(first);
+
+    //////////////////////////GET SECOND////////////////////
+    dot second = *Dots.begin();
+    for (auto dot : Dots) {
+        if (getRotate(first, dot) > getRotate(first, second)) second = dot;
+        if (getRotate(first, dot) == getRotate(first, second)) {
+            if(getLen(first, second)>getLen(first,dot)) second = dot;
+        }
+    }
+    res.push_back(second);
+    Dots.remove(second);
+
+    //////////////////////////GET NEXT/////////////////////
+
+    while (true)
+    {
+        auto iter = res.end();
+        --iter;
+        dot lastRes = *iter;
+        --iter;
+        dot prevLastRes = *iter;
+
+        dot next = *Dots.begin();
+        for(auto i : Dots){
+            if(getCos(prevLastRes, lastRes, next)>getCos(prevLastRes, lastRes, i)) next = i;
+        }
+        if(next == first) return res;
+        else{
+            res.push_back(next);
+            Dots.remove(next);
+        }
+    }
+    
 }
